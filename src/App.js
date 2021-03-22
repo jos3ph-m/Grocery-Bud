99c92d5eabe6a458eef4ba0e61086e4a72879f80
Joseph M <jmedina1985@hotmail.com>
2021-03-22 14:57:32 -0700

    edit an item

--- src/App.js
+++ src/App.js
@@ -42,6 +42,12 @@ function App() {
     setList(list.filter((item) => item.id !== id));
   };
 
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+  };
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
